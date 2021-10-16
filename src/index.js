569d43cc213a37d1e86a715c2aa1f8cd38631c07
Andzelika Domanska <adomanska96@gmail.com>
2021-10-16 18:26:21 +0200

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 
 const fetchSolution = () => console.log('-- Solution will be displayed here --');
 
 const program = new Command();
 
 program
-	.version('1.0.0')
-	.description('Command line Application For Fetching Project Euler Solutions')
+  .version('1.0.0')
+  .description('Command line Application For Fetching Project Euler Solutions');
 
 program
-	.action(fetchSolution);
+  .action(fetchSolution);
 
-program.parse(process.argv)
+program.parse(process.argv);
