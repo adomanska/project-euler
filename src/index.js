08b7bb30f063df291f0618fdfaf31870342400d8
Andzelika Domanska <adomanska96@gmail.com>
2021-10-17 23:30:33 +0200

    Add data and solution fetching

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { Command } from 'commander';
+import * as modulesConfig from './modules/index.js';
+import readLines from './utils/read-lines.js';
 
-const fetchSolution = () => console.log('-- Solution will be displayed here --');
+const fetchSolution = (problemId, dataPath) => {
+  const solve = modulesConfig.default[problemId];
+  const rawData = readLines(dataPath);
+
+  console.log(solve(rawData));
+};
 
 const program = new Command();
 
@@ -9,6 +16,8 @@ program
   .description('Command line Application For Fetching Project Euler Solutions');
 
 program
+  .argument('<problemId>', 'problem id')
+  .argument('[dataPath]', 'path to data file', 'invalid file')
   .action(fetchSolution);
 
 program.parse(process.argv);
