5516bbc45bf726e4b5a33aceb785517de10b9597
adomanska <adomanska96@gmail.com>
2021-10-26 21:31:00 +0200

    Add solver with tests

---
+++ src/modules/lexicographic-permutations/__tests__/index.test.ts
@@ -0,0 +1,30 @@
+import { parseData, solve } from '..';
+
+describe('parseData', () => {
+  it('should return parsed data', () => {
+    const rawData = [
+      '0,1,2',
+      '4',
+    ];
+
+    const data = parseData(rawData);
+
+    expect(data).toEqual({
+      elements: ['0', '1', '2'],
+      index: 4,
+    });
+  });
+});
+
+describe('solve', () => {
+  it('should return element of lexicographic permuation at given index', () => {
+    const data = {
+      elements: ['0', '1', '2'],
+      index: 4,
+    };
+
+    const result = solve(data);
+
+    expect(result).toEqual('120');
+  });
+});
