d3cbf15d2a5527cc4f3d581cf7e05ad01efed187
adomanska <adomanska96@gmail.com>
2021-12-14 20:51:47 +0100

    Simplify solver

--- src/modules/distinct-powers/index.ts
+++ src/modules/distinct-powers/index.ts
@@ -1,17 +1,15 @@
 import parseData from '../../common/parse-single-number';
 
 export const solve = (max: number) => {
-  const distinctPowers = new Array(max + 1);
-  distinctPowers.fill(0);
+  let duplicatesCount = 0;
 
   for (let index = 2; index <= max; index += 1) {
-    distinctPowers[index] += (max - 1);
     for (let power = 2; index ** power <= max; power += 1) {
-      distinctPowers[index ** power] -= (Math.floor(max / power) - 1);
+      duplicatesCount += (Math.floor(max / power) - 1);
     }
   }
 
-  return distinctPowers.reduce((acc, cur) => acc + cur);
+  return (max - 1) ** 2 - duplicatesCount;
 };
 
 const solveProblem = (rawData: string[]) => solve(parseData(rawData)).toString();
